11ed453e9affe1a46fabf6be424dc7b82a85d40c
Qiang Ye <yeqiang2k@hotmail.com>
2015-12-01 13:55:27 -0400

    1st Version

---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+public class HelloWorld {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		
+		File f = new File(args[0]);
+		
+		int i=5;
+		
+		int j=6;
+		
+		i=j;
+
+	}
+
+	
+	
+
+}
---
+++ src/secondClasss.java
@@ -0,0 +1,24 @@
+
+public class secondClasss {
+	
+	int i;
+	
+	int j;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+}
